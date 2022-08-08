17551ac5b991cc9ddd15cea8728e7083855373dd
Lucas Calderon <lucas8calderon@hotmail.com>
2022-08-08 17:06:52 -0300

    first commit

---
+++ app/src/main/java/com/lucascalderon1/playvideo/PlayerActivity.java
@@ -0,0 +1,37 @@
+package com.lucascalderon1.playvideo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class PlayerActivity extends AppCompatActivity {
+
+
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        videoView = findViewById(R.id.videoView);
+
+        View decorView = getWindow().getDecorView();
+        int uiOpcoes = View.SYSTEM_UI_FLAG_FULLSCREEN;
+
+        decorView.setSystemUiVisibility(uiOpcoes);
+
+        getSupportActionBar().hide();
+
+        //executar o video
+
+        videoView.setMediaController(new MediaController(this));
+
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.video );
+        videoView.start();
+
+    }
+}
